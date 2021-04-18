e85e08f6187fbce0b8fceb871afb8ee1ec2a993f
Matthew Steglinski <matthew.steglinski@thinkon.com>
2021-04-18 15:56:44 -0400

    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Maste'
+description 'Inventory for players to store items in'
+version '1.0.0'
+
+ui_page 'html/index.html'
+files {
+    'html/*'
+}
+
+client_scripts {
+    'client.lua'
+}
+
